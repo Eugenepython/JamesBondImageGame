929f1fd9edff10f1f5e840e239345f0aaf89f0a7
Eugenepython <50318257+Eugenepython@users.noreply.github.com>
2023-02-05 17:16:33 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,139 @@
+let images = ['images/DN1.jpg', 'images/DN2.jpg', 'images/DN3.jpg', 'images/DN4.jpg', 'images/DN5.jpg', 'images/DN6.png', 'images/DN7.png', 'images/DN8.jpg', 'images/DN9.png',  'images/FRWL1.jpg', 'images/FRWL2.jpg', 'images/FRWL3.jpg', 'images/FRWL4.jpg', 'images/FRWL5.jpg', 'images/FRWL6.jpg', 'images/FRWL7.jpg', 'images/FRWL8.jpg', 'images/FRWL9.jpg', 'images/FRWL10.jpg', "images/GF1.jpg", "images/GF2.jpg", "images/GF3.jpg", "images/GF4.jpg", "images/GF5.jpg", "images/GF6.jpg", "images/GF7.jpg", "images/GF8.jpg", 'images/GF9.jpg', "images/TB1.jpg", "images/TB2.jpg", "images/TB3.jpg", "images/TB4.jpg", "images/TB5.jpg", "images/TB6.jpg", "images/TB7.jpg", "images/TB8.jpg", "images/TB9.jpg", "images/YOLT1.jpg", "images/YOLT2.jpg", "images/YOLT3.jpg", "images/YOLT4.jpg", "images/YOLT5.jpg", "images/YOLT6.jpg", "images/YOLT7.jpg", "images/YOLT8.jpg", "images/YOLT9.jpg", "images/DAF1.jpg", "images/DAF2.jpeg", "images/DAF3.jpg", "images/DAF4.jpeg", "images/DAF5.jpg", "images/DAF6.jpg", "images/DAF7.jpg", "images/DAF8.png", "images/DAF9.png", "images/OHMSS1.jpg", "images/OHMSS2.jpg", "images/OHMSS3.jpg", "images/OHMSS4.jpg", "images/OHMSS5.jpg", "images/OHMSS6.jpg", "images/OHMSS7.png", "images/OHMSS8.jpeg", "images/OHMSS9.jpeg", "images/OHMSS10.jpeg", "images/OHMSS11.png", "images/OHMSS12.jpg", "images/OHMSS13.jpg", "images/OHMSS14.jpg", "images/TMWTGG1.jpg", "images/TMWTGG2.jpg", "images/TMWTGG3.jpg", "images/TMWTGG4.jpg", "images/TMWTGG5.png", "images/TMWTGG6.jpeg", "images/TMWTGG7.jpg", "images/TMWTGG8.jpg", "images/TMWTGG9.png", "images/TMWTGG10.png", "images/TMWTGG11.jpg", "images/LALD1.jpg", "images/LALD2.png", "images/LALD3.jpeg", "images/LALD4.jpeg", "images/LALD5.jpg", "images/LALD6.jpg", "images/LALD7.jpg", "images/LALD8.png", "images/LALD9.jpg", "images/TSWLM1.jpg", "images/TSWLM2.jpg", "images/TSWLM3.jpg", "images/TSWLM4.jpg", "images/TSWLM5.jpeg", "images/TSWLM6.jpeg", "images/TSWLM7.jpg", "images/TSWLM8.jpg", "images/TSWLM9.jpg", "images/TSWLM10.jpg",  "images/MR1.jpg", "images/MR2.jpg", "images/MR3.jpeg", "images/MR4.jpg", "images/MR5.jpg", "images/MR6.jpg", "images/MR7.jpg", "images/MR8.jpg", "images/MR9.jpg", "images/FYEO1.jpg", "images/FYEO2.jpg", "images/FYEO3.jpg", "images/FYEO4.jpg", "images/FYEO5.jpg", "images/FYEO6.png", "images/FYEO7.jpg", "images/FYEO8.jpg", "images/FYEO9.jpg", "images/O1.jpg", "images/O2.jpg", "images/O3.jpg", "images/O4.jpg", "images/O5.jpg", "images/O6.jpg", "images/O7.jpg", "images/O8.jpg", "images/O9.jpg", "images/VTAK1.jpg", "images/VTAK2.jpg", "images/VTAK3.jpg", "images/VTAK4.jpg", "images/VTAK5.png", "images/VTAK6.jpg", "images/VTAK7.jpeg", "images/VTAK8.jpg", "images/VTAK9.jpg", "images/VTAK10.png", "images/NSNA1.png", "images/NSNA2.png", "images/NSNA3.png", "images/NSNA4.jpg", "images/NSNA5.jpg", "images/NSNA6.jpg", "images/NSNA7.jpg", "images/NSNA8.png", "images/NSNA9.jpg", "images/NSNA10.jpg", "images/NSNA11.jpg", "images/NSNA12.jpg", "images/TLD1.jpg", "images/TLD2.jpg", "images/TLD3.jpg", "images/TLD4.jpg", "images/TLD5.jpg", "images/TLD6.jpg", "images/TLD7.jpg", "images/TLD8.png", "images/TLD9.jpg", "images/TLD10.jpg", "images/TLD11.jpg", "images/TLD12.jpg", "images/TLD13.jpg",  "images/TLD14.jpg", "images/LTK1.png", "images/LTK2.jpeg", "images/LTK3.jpg", "images/LTK4.jpeg", "images/LTK5.jpg", "images/LTK6.png", "images/LTK7.jpg", "images/LTK8.jpg", "images/LTK9.jpg", "images/LTK10.jpg", "images/LTK11.jpg", "images/GE1.jpg", "images/GE2.jpg", "images/GE3.jpg", "images/GE4.jpg", "images/GE5.jpg", "images/GE6.jpg", "images/GE7.jpg", "images/GE8.jpg", "images/GE9.jpg", "images/GE10.jpg", "images/GE11.jpg", "images/TND1.jpg", "images/TND2.jpg", "images/TND3.jpg", "images/TND4.png", "images/TND5.jpg", "images/TND6.jpg", "images/TND7.png", "images/TND8.jpg", "images/TWINE1.jpg", "images/TWINE2.jpg", "images/TWINE3.jpg", "images/TWINE4.jpg", "images/TWINE5.jpg", "images/TWINE6.jpg", "images/TWINE7.jpg", "images/TWINE8.png", "images/TWINE9.jpg", "images/TWINE10.jpg", "images/DAD1.jpg", "images/DAD2.jpg", "images/DAD3.jpg", "images/DAD4.jpg", "images/DAD5.jpg", "images/DAD6.jpg", "images/DAD7.jpg", "images/DAD8.jpg", "images/DAD9.jpg", "images/DAD10.jpg",  "images/CR1.png", "images/CR2.jpg", "images/CR3.jpg", "images/CR4.jpg", "images/CR5.jpg", "images/CR6.jpg", "images/CR7.gif", "images/CR8.png", "images/CR9.jpg", "images/CR10.jpeg", 'images/CR11.jpg', "images/S1.jpg", "images/S2.jpg", "images/S3.jpg", "images/S4.jpeg", "images/S5.jpg", "images/S6.jpeg", "images/S7.jpg", "images/S8.png", "images/S9.jpg", "images/SF1.jpg", "images/SF2.jpg", "images/SF3.jpg", "images/SF4.jpeg", "images/SF5.jpg", "images/SF6.jpg", "images/SF7.jpg", "images/SF8.jpg", "images/SF9.jpg", 'images/SF10.jpg', "images/QOS1.jpg", "images/QOS2.jpg", "images/QOS3.jpg", "images/QOS4.jpg", "images/QOS5.jpg", "images/QOS6.png", "images/QOS7.jpg", "images/QOS8.jpeg", "images/QOS9.jpeg", "images/NTTD1.jpeg", "images/NTTD2.jpg", "images/NTTD3.jpeg", "images/NTTD4.jpg", "images/NTTD5.jpg", "images/NTTD6.jpeg", "images/NTTD7.jpg", "images/NTTD8.jpg", "images/NTTD9.jpg", 'images/NTTD10.png', 'images/NTTD10.jpg', 'images/NTTD11.jpg','images/NTTD12.png', 'images/NTTD13.jpeg' ]
+
+document.getElementById('reStart').style.display = 'none';
+let scoreCard = document.getElementById("scoreCard");
+let midArray = images.sort(() => Math.random() - 0.5);
+let newArray = midArray.sort(() => Math.random() - 0.5);
+//console.log(newArray)
+let startButton = document.getElementById("start-button");
+let nextButton = document.getElementById("next-image")
+let card = document.getElementById("card")
+let currentImage
+let resultMessage = document.getElementById("result-message");  
+let rightWwrong = document.getElementById("rightWrong");
+let buttonsId = document.getElementById('buttonsId')
+let image = document.getElementById('image')
+let score = 0;
+let keepScore = document.getElementById('keepScore')
+
+let onePoint = document.getElementById('onePoint')
+let twoPoints = document.getElementById('twoPoints')
+let threePoints = document.getElementById('threePoints')
+let left = document.getElementById('left')
+let percent
+
+
+onePoint.style.display = 'none'
+twoPoints.style.display = 'none'
+let bonus = 3;
+
+buttonsId.style.display = 'none'
+nextButton.style.display = 'none'
+document.getElementById('image-container').style.display = 'none'
+
+startButton.addEventListener("click", function() {
+    document.getElementById('start').style.display = 'none'
+    document.getElementById('image-container').style.display = 'flex'
+    document.getElementById('next-image').style.display = 'block'
+    document.getElementById('buttonsId').style.display = 'block'
+    document.getElementById('start-button').style.display = 'none'
+    nextButton.click();
+});
+
+threePoints.addEventListener("click", function() {
+    threePoints.style.display = 'none'
+    twoPoints.style.display = 'block'
+    image.style.filter = `blur(3px)`;
+    bonus = 2;
+});
+
+twoPoints.addEventListener("click", function() {
+    twoPoints.style.display = 'none'
+    onePoint.style.display = 'block'
+    image.style.filter = `blur(0px)`;
+    bonus = 1;
+});
+
+
+nextButton.addEventListener("click", next);
+let counter = 0;
+
+function next() {
+    eh.style.display = 'none';
+    card.style.display = 'none'
+    bonus = 3;
+    onePoint.style.display = 'none'
+    twoPoints.style.display = 'none'
+    threePoints.style.display = 'block'
+    //keepScore.innerHTML = '007';
+    image.style.filter = `blur(6px)`;
+    rightWwrong.style.display = 'none'
+    buttonsId.style.display = 'block'
+    nextButton.style.display = 'none'
+    image.src = newArray[counter];
+    currentImage = (newArray[counter])
+    counter = (counter + 1) % 16;
+    if (counter === 0) {
+    end.style.display = 'block'
+    resultMessage.innerHTML = `<p>game over</p><p>You scored ${score} out of 45!</p>`
+    document.getElementById('image-container').style.display = 'none'
+    buttonsId.style.display = 'none'
+    nextButton.style.display = 'none'
+    document.getElementById('reStart').style.display = 'block';
+    }
+    //console.log(counter)
+    percent = (counter/15)*100
+    //console.log(percent)
+    }
+
+
+let buttons = document.querySelectorAll("#buttonsId button");
+let i
+buttons.forEach(button => {
+    
+    button.addEventListener('mouseover', function() {
+      this.title = this.dataset.title;
+      this.style.transform = 'scale(1.4)';
+      })
+     button.addEventListener('mouseout', function() {
+    this.style.transform = 'scale(1)';
+    });
+      
+    button.addEventListener("click", function() {
+        eh.style.display = 'block';
+        card.style.display = 'block'
+        i = Math.floor(Math.random() * 4) +1;
+        card.innerHTML = `<img class = 'small' src = 'images/${i}.jpg'>`
+        //console.log(i)
+        onePoint.style.display = 'none'
+        twoPoints.style.display = 'none'
+        threePoints.style.display = 'none'
+        //console.log(this.id + " button was clicked!");
+        image.style.filter = ('blur(0px')
+        buttonsId.style.display = 'none'
+        nextButton.style.display = 'block'
+        rightWrong.style.display = 'block'
+        //console.log(this.id)
+        let pattern = new RegExp(`/${this.id}\\d`);
+        if (pattern.test(currentImage)) {
+            //console.log('ihurrah!');
+            score+=bonus
+            //console.log(score)
+            
+            rightWrong.innerHTML = `<span class="tick">&#10003;</span>` + this.getAttribute("data-title");
+            rightWrong.style.color = 'black'
+            keepScore.innerHTML = score
+            
+        } else {
+            //console.log('not');
+            rightWrong.innerHTML = `<span class="cross">&#10060;</span>` + this.getAttribute("data-title");
+            rightWrong.style.color = 'red'
+            
+        }
+    });
+});
+
+reStart.addEventListener("click", function(){
+    window.location.reload()
+})
+  
